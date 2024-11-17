134c3daa1b504a4f88e13605dc7d09e6fe6e2495
CatPaulKatze <paul.gregor.sommer@gmail.com>
2024-11-17 23:12:31 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-row min-h-screen justify-center">
+        <h1 className="m-5 text-7xl text-cyan-500">HTML 101</h1>
+        <div>
+            
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
